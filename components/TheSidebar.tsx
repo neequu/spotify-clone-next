12c3e74d9f535e3df62154b5da0eb985f789169e
Nick <nist1701@gmail.com>
2023-10-21 22:29:30 +0300

    fix: navbar position in mobile view

--- components/TheSidebar.tsx
+++ components/TheSidebar.tsx
@@ -7,15 +7,13 @@ const TheSidebar = () => {
   return (
     <div className='h-full flex items-center md:items-stretch justify-center md:justify-normal flex-col gap-layout-gap'>
       <TheNavbar />
-      <div className='bg-gray-main flex-1 rounded lg:p-layout-p md:p-3'>
-        <div className='hidden md:block'>
-          <TheLibrary>
-            <div className='text-neutral-400 flex items-center gap-2 font-bold'>
-              <PiPlaylist size={24} />
-              <span className='hidden lg:block'>Your library</span>
-            </div>
-          </TheLibrary>
-        </div>
+      <div className='bg-gray-main flex-1 rounded lg:p-layout-p md:p-3 hidden md:block'>
+        <TheLibrary>
+          <div className='text-neutral-400 flex items-center gap-2 font-bold'>
+            <PiPlaylist size={24} />
+            <span className='hidden lg:block'>Your library</span>
+          </div>
+        </TheLibrary>
       </div>
     </div>
   );
