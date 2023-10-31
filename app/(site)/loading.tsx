c0a37cac44b376e831ae77a5a58fe9f8adcaadae
Nick <nist1701@gmail.com>
2023-10-31 21:45:40 +0300

    refactor: change spinner position

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const loading = () => {
   return (
-    <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-      <Spinner />
+    <div>
+      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Spinner />
+      </div>
     </div>
   );
 };
