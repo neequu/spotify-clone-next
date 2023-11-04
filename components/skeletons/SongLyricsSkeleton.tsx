6443d18c7a7612c1af6ed3d5b4ee1d70279e39e3
neequu <nist1701@gmail.com>
2023-11-04 14:36:07 +0300

    fix: remove dup key

--- components/skeletons/SongLyricsSkeleton.tsx
+++ components/skeletons/SongLyricsSkeleton.tsx
@@ -4,23 +4,11 @@ const SongLyricsSkeleton = () => {
     <div className="grid w-full gap-2">
       {placeholders.map((_, i) => (
         <>
-          <p key={i} className="h-[15px] w-2/5 animate-pulse  bg-neutral-700" />
-          <p
-            key={i}
-            className="h-[15px] w-[55%] animate-pulse  bg-neutral-700"
-          />
-          <p
-            key={i}
-            className="h-[15px] w-[60%] animate-pulse  bg-neutral-700"
-          />
-          <p
-            key={i}
-            className="h-[15px] w-[38%] animate-pulse  bg-neutral-700"
-          />
-          <p
-            key={i}
-            className="h-[15px] w-[42%] animate-pulse  bg-neutral-700"
-          />
+          <p className="h-[15px] w-2/5 animate-pulse  bg-neutral-700" />
+          <p className="h-[15px] w-[55%] animate-pulse  bg-neutral-700" />
+          <p className="h-[15px] w-[60%] animate-pulse  bg-neutral-700" />
+          <p className="h-[15px] w-[38%] animate-pulse  bg-neutral-700" />
+          <p className="h-[15px] w-[42%] animate-pulse  bg-neutral-700" />
         </>
       ))}
     </div>
